de5e7e07d70ec8e9cc022059af80171614c17bf4
Justin Olson <j.olson.digital@gmail.com>
2019-07-27 18:12:34 -0700

    Added hosts file to main folder for testing purposes. Completed 137. Code itself finished.

--- website_blocker.py
+++ website_blocker.py
@@ -1,14 +1,29 @@
 import time
 from datetime import datetime as dt
 
+hosts_temp = "hosts"
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month,
-                                                                          dt.now().day, 16):
-        print("Working!")
+                                                                          dt.now().day, 18):
+        print("Working hours.")
+        with open(hosts_path, "r+") as file:
+            content = file.read()
+            for site in website_list:
+                if site in content:
+                    pass
+                else:
+                    file.write(redirect + " " + site + "\n")
     else:
-        print("Not working!")
+        with open(hosts_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(site in line for site in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Outside working hours.")
     time.sleep(5)
